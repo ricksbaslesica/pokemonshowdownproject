e07238e90a3409f5397c73df1809511ce0471ee1
ricksbaslesica <62109510+ricksbaslesica@users.noreply.github.com>
2020-03-13 12:28:04 -0500

    Add files via upload

--- pokemonclass.py
+++ pokemonclass.py
@@ -16,6 +16,11 @@ class Pokemon:
 			pokemon.specialdefense, pokemon.speed])
 MrRime = Pokemon("Mr. Rime", "Ice", "Psychic", 80, 85, 75, 110, 100, 70)
 Inteleon = Pokemon("Inteleon", "Water", "None", 70, 85, 65, 125, 65, 120)
+Ludicolo = Pokemon("Ludicolo", "Water", "Grass", 80, 70, 70, 90, 100, 70)
+Hawlucha = Pokemon ("Hawlucha", 78, 92, 75, 74, 63, 118)
+Hippowdon = Pokemon("Hippowdon", "Ground", "None", 108, 112, 118, 68, 72, 47)
+Hydreigon = Pokemon("Hydreigon", "Dark", "Dragon", 92, 105, 90, 125, 90, 98)
+Snorlax = Pokemon("Snorlax", "Normal", "None", 160, 110, 65, 65, 110, 30)
 
 def printpokemon(pokemontoprint):
         print (pokemontoprint.speciesname, ":", pokemontoprint.type1, "/", 
